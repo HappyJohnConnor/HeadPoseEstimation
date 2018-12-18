5e0ab996740d0ab8562c771e8c1b521d86f5de6d
jhon_connor <tomoakitaro777@gmail.com>
2018-12-19 00:28:34 +0900

    updated

--- test_model.py
+++ test_model.py
@@ -1,23 +1,23 @@
 import glob
-import data_utils
+from data_maker import utils
 import numpy as np
 
 from keras.models import load_model
-from keras.preprocessing import image
+from keras.preprocessing.image import img_to_array, load_img
 from PIL import Image
 
 from model import googlenet2
 
 if __name__  == '__main__':
     weight_path = './model/output/my_model.hdf5'
-    dataset_path = './dataset/test/AFLW2000'
+    dataset_path = '../dataset/AFLW2000'
 
     img_size = 150
     # モデルをコンパイル
     model = googlenet2.create_googlenet(
         img_size = img_size)
     model.load_weights(weight_path)
-    
+
     positive_num = 0
     test_num = 0
     degree_th = 10
@@ -27,14 +27,17 @@ if __name__  == '__main__':
 
     np_degree = np.arange(-90, 90, 10)
     for mat_file, jpg_image in zip(mat_files, jpg_images):
-        pitch, yaw, roll = data_utils.get_degree_from_mat(mat_file)
+        pitch, yaw, roll = utils.get_degree_from_mat(mat_file)
         
         if abs(pitch) <= degree_th and abs(roll) <= degree_th:
             test_num += 1
-            test_img = np.array(Image.open(jpg_image).resize((img_size, img_size)))
-            test_img = test_img.reshape(
-                (-1, test_img.shape[0], test_img.shape[1], test_img.shape[2]))            
-            results = model.predict(test_img / 255.)
+            print(jpg_image)
+            img = img_to_array(load_img(jpg_image, target_size=(img_size, img_size)))
+            #0-1に変換
+            img_nad = img_to_array(img)/255
+            #4次元配列に
+            img_nad = img_nad[None, ...]
+            results = model.predict(img_nad)
             print(results)
             print('result : ' + str(np.dot(np_degree, results[0])))
             print('correct : ' + str(yaw))
