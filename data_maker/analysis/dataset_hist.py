8f06befd55bc1a8dde5506725765f4d094911884
OmenByHp17 <tomoakitaro777@gmail.com>
2019-01-13 23:06:50 +0900

    roll, pitchを追加

--- data_maker/analysis/dataset_hist.py
+++ data_maker/analysis/dataset_hist.py
@@ -22,40 +22,76 @@ def get_ypr_from_mat(mat_path):
 
 
 pose_ls = []
-dataset_path = '../../dataset/300W_LP'
+df_pose = None
+dataset_path = '../../../dataset/300W_LP'
 dir_path_ls = ['AFW', 'AFW_Flip',
                'HELEN', 'HELEN_Flip',
                'IBUG', 'IBUG_Flip',
                'LFPW', 'LFPW_Flip']
+csv_file_name = 'df_300w_lp.csv'
+csv_path = './'+ csv_file_name
 """
 dataset_path = '../../dataset/AFLW2000'
 dir_path_ls = ['.']
 """
+# CSVファイルの存在の確認
+if os.path.exists(csv_path):
+    df_pose = pd.read_csv(csv_path)
+else:
+    for each_dir in dir_path_ls:
+        dir_path = os.path.join(dataset_path, each_dir)
+        files = glob.glob(dir_path+'/*.mat')
 
-for each_dir in dir_path_ls:
-    dir_path = os.path.join(dataset_path, each_dir)
-    files = glob.glob(dir_path+'/*.mat')
+        for file in files:
+            pose_params = get_ypr_from_mat(file)
+            pose_ls.append(pose_params)
 
-    for file in files:
-        pose_params = get_ypr_from_mat(file)
-        pose_ls.append(pose_params)
+    # 度数に変換
+    pitch = [pose[0] * 180 / np.pi for pose in pose_ls]
+    yaw = [pose[1] * 180 / np.pi for pose in pose_ls]
+    roll = [pose[2] * 180 / np.pi for pose in pose_ls]
 
-# 度数に変換
-pitch = [pose[0] * 180 / np.pi for pose in pose_ls]
-yaw = [pose[1] * 180 / np.pi for pose in pose_ls]
-roll = [pose[2] * 180 / np.pi for pose in pose_ls]
+    df_pose = pd.DataFrame(
+        {"pitch": pitch, 'yaw': yaw, 'roll': roll}
+    )
 
-df_pose = pd.DataFrame(
-    {"pitch": pitch, 'yaw': yaw, 'roll': roll}
-)
+    # CSVに出力
+    df_pose.to_csv(csv_file_name)
+
+# YAW
 # 絶対値10度以上のやつを除外
-picked_pose = (np.fabs(df_pose['pitch']) <= 10) & (
+yaw_pose = (np.fabs(df_pose['pitch']) <= 10) & (
     np.fabs(df_pose['roll']) <= 10)
-df_pose = df_pose[picked_pose]
-print(df_pose)
+df_pose = df_pose[yaw_pose]
 
 df_pose['yaw'].hist(bins=18)
 plt.xlabel("degree")
 plt.ylabel('number')
 plt.xticks(np.arange(-90, 90, 10))
-plt.savefig('fig1.png')
+plt.savefig('yaw_hist.png')
+
+# ROLL
+# 絶対値10度以上のやつを除外
+plt.figure()
+roll_pose = (np.fabs(df_pose['yaw']) <= 10) & (
+    np.fabs(df_pose['pitch']) <= 10)
+df_pose = df_pose[roll_pose]
+
+df_pose['roll'].hist(bins=18)
+plt.xlabel("degree")
+plt.ylabel('number')
+plt.xticks(np.arange(-90, 90, 10))
+plt.savefig('roll_hist.png')
+
+# PITCH
+# 絶対値10度以上のやつを除外
+plt.figure()
+pitch_pose = (np.fabs(df_pose['yaw']) <= 10) & (
+    np.fabs(df_pose['roll']) <= 10)
+df_pose = df_pose[pitch_pose]
+
+df_pose['pitch'].hist(bins=18)
+plt.xlabel("degree")
+plt.ylabel('number')
+plt.xticks(np.arange(-90, 90, 10))
+plt.savefig('pitch_hist.png')
